055b68271a1bab45920fca0533ef1b57d01e7d0d
kpg1997 <kpg19979@gmail.com>
2021-02-17 15:37:42 +0900

    test

--- web.js
+++ web.js
@@ -38,7 +38,7 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.json());
 
-app.listen(8020, function () {
+app.listen(8002, function () {
     console.log('server running at http://127.0.0.1:8020');
 });
 //미들웨어 장착
